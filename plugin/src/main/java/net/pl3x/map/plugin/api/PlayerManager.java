afe5281d09e2a555fc1edf198d94e84649ebbbfe
BillyGalbreath <blake.galbreath@gmail.com>
2021-02-06 19:00:48 -0600

    use PDC for persistent hidden status

--- plugin/src/main/java/net/pl3x/map/plugin/api/PlayerManager.java
+++ plugin/src/main/java/net/pl3x/map/plugin/api/PlayerManager.java
@@ -1,22 +1,26 @@
 package net.pl3x.map.plugin.api;
 
+import net.pl3x.map.plugin.Pl3xMapPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class PlayerManager implements net.pl3x.map.api.PlayerManager {
-    private final Set<UUID> hiddenPlayers = ConcurrentHashMap.newKeySet();
+    public static NamespacedKey key = new NamespacedKey(Pl3xMapPlugin.getInstance(), "hidden");
 
     @Override
     public void hide(final @NonNull UUID uuid) {
-        this.hiddenPlayers.add(uuid);
+        pdc(uuid).set(key, PersistentDataType.BYTE, (byte) 1);
     }
 
     @Override
     public void show(final @NonNull UUID uuid) {
-        this.hiddenPlayers.remove(uuid);
+        pdc(uuid).set(key, PersistentDataType.BYTE, (byte) 0);
     }
 
     @Override
@@ -30,6 +34,14 @@ public class PlayerManager implements net.pl3x.map.api.PlayerManager {
 
     @Override
     public boolean hidden(final @NonNull UUID uuid) {
-        return this.hiddenPlayers.contains(uuid);
+        return pdc(uuid).getOrDefault(key, PersistentDataType.BYTE, (byte) 0) != (byte) 0;
+    }
+
+    private static PersistentDataContainer pdc(UUID uuid) {
+        Player player = Bukkit.getPlayer(uuid);
+        if (player == null) {
+            throw new IllegalArgumentException("Player is not online");
+        }
+        return player.getPersistentDataContainer();
     }
 }
