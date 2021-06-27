beca085d5244b8ed1393a90d47d9af07e4656cad
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-26 21:16:18 -0700

    Update Paperweight to 1.1.7

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,16 +1,10 @@
-import io.papermc.paperweight.tasks.BaseTask
+import io.papermc.paperweight.tasks.RemapJar
 import io.papermc.paperweight.util.Constants
-import io.papermc.paperweight.util.Constants.paperTaskOutput
-import io.papermc.paperweight.util.cache
-import io.papermc.paperweight.util.defaultOutput
-import io.papermc.paperweight.util.ensureDeleted
-import io.papermc.paperweight.util.ensureParentExists
 import io.papermc.paperweight.util.registering
-import io.papermc.paperweight.util.runJar
 
 plugins {
     id("com.github.johnrengelman.shadow") version "7.0.0"
-    id("io.papermc.paperweight.patcher") version "1.1.6"
+    id("io.papermc.paperweight.patcher") version "1.1.7"
     id("net.minecrell.plugin-yml.bukkit") version "0.4.0"
     id("xyz.jpenilla.run-paper") version "1.0.3"
 }
@@ -48,7 +42,7 @@ tasks {
             "org.bstats"
         ).forEach { relocate(it, "${rootProject.group}.plugin.lib.$it") }
     }
-    val productionMappedJar by registering<RemapJar2> {
+    val productionMappedJar by registering<RemapJar> {
         inputJar.set(shadowJar.flatMap { it.archiveFile })
         outputJar.set(project.layout.buildDirectory.file("libs/${rootProject.name}-${rootProject.version}.jar"))
         mappingsFile.set(project.layout.projectDirectory.file("mojang+yarn-spigot-reobf-patched.tiny"))
@@ -76,98 +70,4 @@ bukkit {
     apiVersion = "1.17"
     website = project.property("githubUrl") as String
     authors = listOf("BillyGalbreath", "jmp")
-}
-
-/*
- * paperweight is a Gradle plugin for the PaperMC project.
- *
- * Copyright (c) 2021 Kyle Wood (DemonWav)
- *                    Contributors
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation;
- * version 2.1 only, no later versions.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- * USA
- */
-/**
- * Copy pasted from Paperweight [io.papermc.paperweight.tasks.RemapJar]
- *
- * [https://github.com/PaperMC/paperweight/pull/44]
- */
-abstract class RemapJar2 : BaseTask() {
-
-    @get:InputFile
-    abstract val inputJar: RegularFileProperty
-
-    @get:InputFile
-    abstract val mappingsFile: RegularFileProperty
-
-    @get:Input
-    abstract val fromNamespace: Property<String>
-
-    @get:Input
-    abstract val toNamespace: Property<String>
-
-    @get:Input
-    abstract val rebuildSourceFilenames: Property<Boolean>
-
-    @get:Internal
-    abstract val jvmargs: ListProperty<String>
-
-    @get:Internal
-    abstract val singleThreaded: Property<Boolean>
-
-    @get:Classpath
-    abstract val remapper: ConfigurableFileCollection
-
-    @get:OutputFile
-    abstract val outputJar: RegularFileProperty
-
-    // Pl3xMap start
-    @get:InputFiles
-    abstract val remapClasspath: ConfigurableFileCollection
-    // Pl3xmap end
-
-    override fun init() {
-        outputJar.convention(defaultOutput())
-        singleThreaded.convention(true)
-        jvmargs.convention(listOf("-Xmx1G"))
-        rebuildSourceFilenames.convention(true)
-    }
-
-    @TaskAction
-    fun run() {
-        val logFile = layout.cache.resolve(paperTaskOutput("log"))
-        ensureDeleted(logFile)
-
-        val args = mutableListOf(
-            inputJar.get().asFile.absolutePath,
-            outputJar.get().asFile.absolutePath,
-            mappingsFile.get().asFile.absolutePath,
-            fromNamespace.get(),
-            toNamespace.get(),
-            *remapClasspath.asFileTree.map { it.absolutePath }.toTypedArray(), // Pl3xMap
-            "--fixpackageaccess",
-            "--renameinvalidlocals"
-        )
-        if (singleThreaded.get()) {
-            args += "--threads=1"
-        }
-        if (rebuildSourceFilenames.get()) {
-            args += "--rebuildsourcefilenames"
-        }
-
-        ensureParentExists(logFile)
-        runJar(remapper, layout.cache, logFile, jvmArgs = jvmargs.get(), args = args.toTypedArray())
-    }
 }
