3c9f0a59caf1cfc1fc4e1cf892bbc47224df44df
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-12-18 03:33:46 -0800

    update cloud to 1.7.0-SNAPSHOT
    fixes #8

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     paperDevBundle("1.18.1-R0.1-SNAPSHOT")
 
     implementation(project(":squaremap-api"))
-    implementation(platform("cloud.commandframework:cloud-bom:1.6.0"))
+    implementation(platform("cloud.commandframework:cloud-bom:1.7.0-SNAPSHOT"))
     implementation("cloud.commandframework", "cloud-paper")
     implementation("cloud.commandframework", "cloud-minecraft-extras") {
         isTransitive = false // Paper provides adventure
