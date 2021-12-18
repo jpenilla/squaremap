2f41e71e304d3b11e9cf8860c94cc8e3e3f377cf
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-12-18 03:33:57 -0800

    update shadow to 7.1.1

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
     id("io.papermc.paperweight.userdev") version "1.3.2"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
     id("xyz.jpenilla.run-paper") version "1.0.6"
