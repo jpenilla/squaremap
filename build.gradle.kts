21fd26a32b0e081269bb6a5217a657eaa2d692e8
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-01-02 12:51:05 -0800

    Fix group version and description in subprojects
    was handled by indra publishing, which we don't apply to all projects anymore

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
   base
 }
 
-group = "xyz.jpenilla"
-version = "1.1.0-SNAPSHOT"
-description = "Minimalistic and lightweight world map viewer for Paper servers"
+allprojects {
+  group = "xyz.jpenilla"
+  version = "1.1.0-SNAPSHOT"
+  description = "Minimalistic and lightweight world map viewer for Paper servers"
+}
 
 subprojects {
   apply(plugin = "base-conventions")
