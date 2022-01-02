d96b9e03db1e87b997311ffdb2de17f965d777ca
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-01-02 11:39:13 -0800

    adjust world load listener priorities

--- plugin/src/main/java/xyz/jpenilla/squaremap/plugin/listener/WorldEventListener.java
+++ plugin/src/main/java/xyz/jpenilla/squaremap/plugin/listener/WorldEventListener.java
@@ -2,6 +2,7 @@ package xyz.jpenilla.squaremap.plugin.listener;
 
 import org.bukkit.craftbukkit.v1_18_R1.CraftWorld;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
@@ -10,22 +11,22 @@ import xyz.jpenilla.squaremap.common.config.WorldConfig;
 import xyz.jpenilla.squaremap.plugin.SquaremapPlugin;
 
 public final class WorldEventListener implements Listener {
-
     private final SquaremapPlugin plugin;
 
     public WorldEventListener(final @NonNull SquaremapPlugin plugin) {
         this.plugin = plugin;
     }
 
-    @EventHandler
+    // Use low priority to load world before other plugins load listeners
+    @EventHandler(priority = EventPriority.LOW)
     public void handleWorldLoad(final @NonNull WorldLoadEvent event) {
         WorldConfig.get(((CraftWorld) event.getWorld()).getHandle());
         this.plugin.worldManager().getWorldIfEnabled(event.getWorld());
     }
 
-    @EventHandler
+    // Use high priority to unload world after other plugins unload listeners
+    @EventHandler(priority = EventPriority.HIGH)
     public void handleWorldUnload(final @NonNull WorldUnloadEvent event) {
         this.plugin.worldManager().worldUnloaded(event.getWorld());
     }
-
 }
