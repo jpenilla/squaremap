3c4f88b6f3ead871a7a5100f7dfb7840cfe59603
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-01-03 18:35:57 -0800

    adjust BlockItemMixin

--- fabric/src/main/java/xyz/jpenilla/squaremap/fabric/mixin/BlockItemMixin.java
+++ fabric/src/main/java/xyz/jpenilla/squaremap/fabric/mixin/BlockItemMixin.java
@@ -16,7 +16,7 @@ abstract class BlockItemMixin {
         method = "place(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/InteractionResult;",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/item/BlockItem;updateBlockStateFromTag(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/block/state/BlockState;)Lnet/minecraft/world/level/block/state/BlockState;"
+            target = "Lnet/minecraft/world/level/Level;gameEvent(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/core/BlockPos;)V"
         )
     )
     void injectPlace(BlockPlaceContext blockPlaceContext, CallbackInfoReturnable<InteractionResult> cir) {
