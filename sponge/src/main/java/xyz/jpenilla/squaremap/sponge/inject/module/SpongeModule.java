6f336a01813a2b0479df49c4c4b48362dac0b4bf
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-03-24 14:30:59 -0700

    sponge: Use new ComponentFlattener api

--- sponge/src/main/java/xyz/jpenilla/squaremap/sponge/inject/module/SpongeModule.java
+++ sponge/src/main/java/xyz/jpenilla/squaremap/sponge/inject/module/SpongeModule.java
@@ -2,17 +2,14 @@ package xyz.jpenilla.squaremap.sponge.inject.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import java.lang.reflect.Field;
 import java.nio.file.Path;
 import net.kyori.adventure.text.flattener.ComponentFlattener;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
+import org.spongepowered.api.adventure.SpongeComponents;
 import xyz.jpenilla.squaremap.common.ServerAccess;
 import xyz.jpenilla.squaremap.common.command.PlatformCommands;
 import xyz.jpenilla.squaremap.common.inject.annotation.DataDirectory;
-import xyz.jpenilla.squaremap.common.util.ReflectionUtil;
-import xyz.jpenilla.squaremap.common.util.Util;
 import xyz.jpenilla.squaremap.sponge.SpongeServerAccess;
 import xyz.jpenilla.squaremap.sponge.command.SpongeCommands;
 
@@ -39,12 +36,6 @@ public final class SpongeModule extends AbstractModule {
 
     @Provides
     public ComponentFlattener componentFlattener() {
-        final PlainTextComponentSerializer plainText = PlainTextComponentSerializer.plainText();
-        final Field flattener = ReflectionUtil.needField(plainText.getClass(), "flattener");
-        try {
-            return (ComponentFlattener) flattener.get(plainText);
-        } catch (final IllegalAccessException ex) {
-            throw Util.rethrow(ex);
-        }
+        return SpongeComponents.flattener();
     }
 }
