8f6b8d0c5d1caa93f91e98aaf8590afb5fd08527
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-06-23 18:29:52 -0700

    Improve the VanillaChunkSnapshotProvider

--- common/src/main/java/xyz/jpenilla/squaremap/common/util/VanillaChunkSnapshotProvider.java
+++ common/src/main/java/xyz/jpenilla/squaremap/common/util/VanillaChunkSnapshotProvider.java
@@ -2,7 +2,6 @@ package xyz.jpenilla.squaremap.common.util;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.mojang.datafixers.util.Either;
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
@@ -11,6 +10,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkStatus;
+import net.minecraft.world.level.chunk.ImposterProtoChunk;
 import net.minecraft.world.level.chunk.LevelChunk;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
@@ -40,43 +40,51 @@ public final class VanillaChunkSnapshotProvider implements ChunkSnapshotProvider
     }
 
     private static @Nullable LevelChunk fullChunkIfGenerated(final ServerLevel level, final int x, final int z) {
-        // below casts to LevelChunk are safe as full chunks should always be LevelChunks
-
         final ChunkPos chunkPos = new ChunkPos(x, z);
         final ChunkMapAccess chunkMap = (ChunkMapAccess) level.getChunkSource().chunkMap;
 
         final ChunkHolder visibleChunk = chunkMap.squaremap$getVisibleChunkIfPresent(chunkPos.toLong());
         if (visibleChunk != null) {
-            final @Nullable ChunkAccess chunk = fullIfPresent(visibleChunk);
+            final @Nullable LevelChunk chunk = fullIfPresent(visibleChunk);
             if (chunk != null) {
-                return (LevelChunk) chunk;
+                return chunk;
             }
         }
 
         final ChunkHolder unloadingChunk = chunkMap.squaremap$pendingUnloads().get(chunkPos.toLong());
         if (unloadingChunk != null) {
-            final @Nullable ChunkAccess chunk = fullIfPresent(unloadingChunk);
+            final @Nullable LevelChunk chunk = fullIfPresent(unloadingChunk);
             if (chunk != null) {
-                return (LevelChunk) chunk;
+                return chunk;
             }
         }
 
         final @Nullable CompoundTag chunkTag = chunkMap.squaremap$readChunk(chunkPos).join().orElse(null);
         if (chunkTag != null && chunkTag.contains("Status", Tag.TAG_STRING)) {
             if (ChunkStatus.FULL.getName().equals(chunkTag.getString("Status"))) {
-                return (LevelChunk) level.getChunkSource()
-                    .getChunkFuture(x, z, ChunkStatus.FULL, true)
+                @Nullable ChunkAccess chunk = level.getChunkSource()
+                    .getChunkFuture(x, z, ChunkStatus.EMPTY, true)
                     .join()
                     .left()
                     .orElse(null);
+                return unwrap(chunk);
             }
         }
 
         return null;
     }
 
-    private static @Nullable ChunkAccess fullIfPresent(final ChunkHolder chunkHolder) {
-        final CompletableFuture<Either<ChunkAccess, ChunkHolder.ChunkLoadingFailure>> future = chunkHolder.getFutureIfPresentUnchecked(ChunkStatus.FULL);
-        return future.getNow(ChunkHolder.UNLOADED_CHUNK).left().orElse(null);
+    private static @Nullable LevelChunk fullIfPresent(final ChunkHolder chunkHolder) {
+        return unwrap(chunkHolder.getLastAvailable());
+    }
+
+    private static @Nullable LevelChunk unwrap(@Nullable ChunkAccess chunk) {
+        if (chunk == null || !chunk.getStatus().isOrAfter(ChunkStatus.FULL)) {
+            return null;
+        }
+        if (chunk instanceof ImposterProtoChunk imposter) {
+            chunk = imposter.getWrapped();
+        }
+        return (LevelChunk) chunk;
     }
 }
