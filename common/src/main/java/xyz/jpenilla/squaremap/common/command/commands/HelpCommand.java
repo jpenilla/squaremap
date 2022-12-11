123b409d342db5da0e03cb55a61022ab71acf1b0
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-12-11 12:00:22 -0700

    Clean up HelpCommand query argument

--- common/src/main/java/xyz/jpenilla/squaremap/common/command/commands/HelpCommand.java
+++ common/src/main/java/xyz/jpenilla/squaremap/common/command/commands/HelpCommand.java
@@ -2,6 +2,7 @@ package xyz.jpenilla.squaremap.common.command.commands;
 
 import cloud.commandframework.CommandHelpHandler;
 import cloud.commandframework.CommandManager;
+import cloud.commandframework.arguments.CommandArgument;
 import cloud.commandframework.arguments.standard.StringArgument;
 import cloud.commandframework.context.CommandContext;
 import cloud.commandframework.minecraft.extras.AudienceProvider;
@@ -9,6 +10,8 @@ import cloud.commandframework.minecraft.extras.MinecraftExtrasMetaKeys;
 import cloud.commandframework.minecraft.extras.MinecraftHelp;
 import cloud.commandframework.minecraft.extras.RichDescription;
 import com.google.inject.Inject;
+import java.util.List;
+import java.util.function.BiFunction;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
@@ -25,41 +28,43 @@ import xyz.jpenilla.squaremap.common.util.Components;
 @DefaultQualifier(NonNull.class)
 public final class HelpCommand extends SquaremapCommand {
     private final MinecraftHelp<Commander> minecraftHelp;
+    private final CommandArgument<Commander, String> helpQueryArgument;
 
     @Inject
     private HelpCommand(final Commands commands) {
         super(commands);
         this.minecraftHelp = createMinecraftHelp(commands.commandManager());
+        this.helpQueryArgument = createHelpQueryArgument(commands);
     }
 
     @Override
     public void register() {
-        final var commandHelpHandler = this.commands.commandManager().createCommandHelpHandler();
-        final var helpQueryArgument = StringArgument.<Commander>newBuilder("query")
-            .greedy()
-            .asOptional()
-            .withSuggestionsProvider((context, input) -> {
-                final var indexHelpTopic = (CommandHelpHandler.IndexHelpTopic<Commander>) commandHelpHandler.queryHelp(context.getSender(), "");
-                return indexHelpTopic.getEntries()
-                    .stream()
-                    .map(CommandHelpHandler.VerboseHelpEntry::getSyntaxString)
-                    .toList();
-            })
-            .build();
-
         this.commands.registerSubcommand(builder ->
             builder.literal("help")
                 .meta(MinecraftExtrasMetaKeys.DESCRIPTION, Messages.HELP_COMMAND_DESCRIPTION.asComponent())
-                .argument(helpQueryArgument, RichDescription.of(Messages.HELP_QUERY_ARGUMENT_DESCRIPTION))
+                .argument(this.helpQueryArgument, RichDescription.of(Messages.HELP_QUERY_ARGUMENT_DESCRIPTION))
                 .permission("squaremap.command.help")
                 .handler(this::executeHelp));
     }
 
     private void executeHelp(final CommandContext<Commander> context) {
         this.minecraftHelp.queryCommands(
-            context.<String>getOptional("query").orElse(""),
+            context.getOptional(this.helpQueryArgument).orElse(""),
             context.getSender()
         );
+    }
+
+    private static CommandArgument<Commander, String> createHelpQueryArgument(final Commands commands) {
+        final var commandHelpHandler = commands.commandManager().createCommandHelpHandler();
+        final BiFunction<CommandContext<Commander>, String, List<String>> suggestions = (context, input) ->
+            commandHelpHandler.queryRootIndex(context.getSender()).getEntries().stream()
+                .map(CommandHelpHandler.VerboseHelpEntry::getSyntaxString)
+                .toList();
+        return StringArgument.<Commander>newBuilder("query")
+            .greedy()
+            .withSuggestionsProvider(suggestions)
+            .asOptional()
+            .build();
     }
 
     private static MinecraftHelp<Commander> createMinecraftHelp(final CommandManager<Commander> manager) {
