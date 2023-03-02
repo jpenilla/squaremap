bd705543e502f66c36454547047422034e8c2cbc
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-03-02 12:35:04 -0700

    Add more forge map update listeners

--- forge/src/main/java/xyz/jpenilla/squaremap/forge/event/ForgeMapUpdates.java
+++ forge/src/main/java/xyz/jpenilla/squaremap/forge/event/ForgeMapUpdates.java
@@ -1,12 +1,21 @@
 package xyz.jpenilla.squaremap.forge.event;
 
 import com.google.inject.Inject;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.block.piston.PistonStructureResolver;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.BlockSnapshot;
 import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.event.level.ExplosionEvent;
+import net.minecraftforge.event.level.PistonEvent;
+import net.minecraftforge.event.level.SaplingGrowTreeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
 import xyz.jpenilla.squaremap.common.data.ChunkCoordinate;
@@ -22,19 +31,55 @@ public final class ForgeMapUpdates {
     }
 
     public void register() {
-        MinecraftForge.EVENT_BUS.addListener((BlockEvent.BreakEvent event) -> {
+        List.of(
+            BlockEvent.BreakEvent.class,
+            BlockEvent.EntityPlaceEvent.class,
+            BlockEvent.CropGrowEvent.Post.class,
+            BlockEvent.BlockToolModificationEvent.class,
+            BlockEvent.EntityMultiPlaceEvent.class,
+            BlockEvent.FarmlandTrampleEvent.class,
+            BlockEvent.FluidPlaceBlockEvent.class,
+            BlockEvent.PortalSpawnEvent.class
+        ).forEach(cls -> MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, cls, event -> {
+            if (!(event.getLevel() instanceof ServerLevel level)) {
+                return;
+            }
+            if (event instanceof BlockEvent.EntityMultiPlaceEvent multiPlace) {
+                multiPlace.getReplacedBlockSnapshots().stream().map(BlockSnapshot::getPos)
+                    .forEach(pos -> this.markBlock(level, event.getPos()));
+            }
+            this.markBlock(level, event.getPos());
+        }));
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, (BlockEvent.CreateFluidSourceEvent event) -> {
             if (!(event.getLevel() instanceof ServerLevel level)) {
                 return;
             }
             this.markBlock(level, event.getPos());
         });
-        MinecraftForge.EVENT_BUS.addListener((BlockEvent.EntityPlaceEvent event) -> {
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, (ExplosionEvent.Detonate event) -> {
+            if (!(event.getLevel() instanceof ServerLevel level)) {
+                return;
+            }
+            for (final BlockPos affectedBlock : event.getAffectedBlocks()) {
+                this.markBlock(level, affectedBlock);
+            }
+        });
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, (SaplingGrowTreeEvent event) -> {
             if (!(event.getLevel() instanceof ServerLevel level)) {
                 return;
             }
             this.markBlock(level, event.getPos());
         });
-        MinecraftForge.EVENT_BUS.addListener((ChunkGenerateEvent event) -> this.markChunk(event.level(), event.chunkPos()));
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, (PistonEvent.Post event) -> {
+            if (!(event.getLevel() instanceof ServerLevel level)) {
+                return;
+            }
+            final PistonStructureResolver structureHelper = Objects.requireNonNull(event.getStructureHelper());
+            Stream.concat(structureHelper.getToPush().stream(), structureHelper.getToDestroy().stream())
+                .forEach(pos -> this.markBlock(level, pos));
+            this.markBlock(level, event.getPos());
+        });
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, (ChunkGenerateEvent event) -> this.markChunk(event.level(), event.chunkPos()));
     }
 
     private void markBlock(final ServerLevel level, final BlockPos pos) {
