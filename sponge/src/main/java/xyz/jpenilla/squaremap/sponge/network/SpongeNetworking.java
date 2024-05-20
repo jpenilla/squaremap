967f98e29adf05d9e84930aa3c0445e5cd9227e4
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-05-19 20:26:35 -0700

    Fix sponge compile

--- sponge/src/main/java/xyz/jpenilla/squaremap/sponge/network/SpongeNetworking.java
+++ sponge/src/main/java/xyz/jpenilla/squaremap/sponge/network/SpongeNetworking.java
@@ -3,18 +3,17 @@ package xyz.jpenilla.squaremap.sponge.network;
 import com.google.inject.Inject;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
 import org.spongepowered.api.ResourceKey;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.entity.ChangeEntityWorldEvent;
 import org.spongepowered.api.event.network.ServerSideConnectionEvent;
-import org.spongepowered.api.network.EngineConnectionSide;
-import org.spongepowered.api.network.ServerSideConnection;
+import org.spongepowered.api.network.EngineConnectionState;
 import org.spongepowered.api.network.channel.ChannelBuf;
 import org.spongepowered.api.network.channel.ChannelManager;
 import org.spongepowered.api.network.channel.raw.RawDataChannel;
+import org.spongepowered.api.profile.GameProfile;
 import xyz.jpenilla.squaremap.common.network.NetworkingHandler;
 import xyz.jpenilla.squaremap.common.util.Util;
 
@@ -31,14 +30,11 @@ public final class SpongeNetworking {
         channelManager.ofType(
             (ResourceKey) (Object) NetworkingHandler.CHANNEL,
             RawDataChannel.class
-        ).play().addHandler(EngineConnectionSide.SERVER, this::handleIncoming);
+        ).play().addHandler(EngineConnectionState.Game.class, this::handleIncoming);
     }
 
-    private void handleIncoming(final ChannelBuf data, final ServerSideConnection connection) {
-        if (!(connection instanceof ServerGamePacketListenerImpl listener)) {
-            return;
-        }
-        this.networking.handleIncoming(listener.getPlayer(), Util.raw((FriendlyByteBuf) data), map -> true);
+    private void handleIncoming(final ChannelBuf data, final EngineConnectionState.Game state) {
+        this.networking.handleIncoming((ServerPlayer) state.player(), Util.raw((FriendlyByteBuf) data), map -> true);
     }
 
     @Listener
@@ -50,6 +46,6 @@ public final class SpongeNetworking {
 
     @Listener
     public void disconnect(final ServerSideConnectionEvent.Disconnect event) {
-        this.networking.onDisconnect(event.player().uniqueId());
+        event.profile().map(GameProfile::uuid).ifPresent(this.networking::onDisconnect);
     }
 }
