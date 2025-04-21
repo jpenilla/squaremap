fc968a432637f1b77a9ed1b70e2de7654bd299b0
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-04-21 16:48:59 -0700

    pushState instead of replaceState

--- common/src/main/resources/web/js/modules/UILink.js
+++ common/src/main/resources/web/js/modules/UILink.js
@@ -13,7 +13,7 @@ class UILink {
                 this._link.innerHTML = `<img src='images/clear.png'/>`;
                 this._link.onclick = async () => {
                     const url = P.worldList.curWorld == null ? "" : P.getUrlFromView();
-                    window.history.replaceState(null, "", url);
+                    window.history.pushState(null, "", url);
                     await navigator.clipboard.writeText(window.location.href);
                 };
                 if (!show) {
