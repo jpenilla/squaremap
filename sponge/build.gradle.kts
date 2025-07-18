bdcbaa61f6e65284b817b0617b47a96034bcace0
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-07-17 20:55:54 -0700

    Build with 1.21.8

--- sponge/build.gradle.kts
+++ sponge/build.gradle.kts
@@ -32,7 +32,7 @@ configurations.spongeRuntime {
     resolutionStrategy {
         eachDependency {
             if (target.name == "spongevanilla") {
-                useVersion("1.21.7-16.+")
+                useVersion("1.21.8-16.+")
             }
         }
     }
